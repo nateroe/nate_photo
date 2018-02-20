730dc12eda4db0eaf63fa7582377e205cfd4c7ea
nate <phlake@gmail.com>
2018-02-20 09:27:45 -0800

    GPL legal statements to all source code, clean up license

--- BackEnd/src/main/java/com/nateroe/photo/model/Role.java
+++ BackEnd/src/main/java/com/nateroe/photo/model/Role.java
@@ -1,5 +1,25 @@
 package com.nateroe.photo.model;
 
+/**
+ * NatePhoto - A photo catalog and presentation application.
+ * Copyright (C) 2018 Nathaniel Roe
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contact nate [at] nateroe [dot] com
+ */
+
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
