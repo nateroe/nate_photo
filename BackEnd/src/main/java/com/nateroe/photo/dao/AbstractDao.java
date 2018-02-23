1394250ca8fac7074bf9405adcb68eca36cff899
nate <phlake@gmail.com>
2018-02-23 14:07:56 -0800

    abstract class is not @Stateless

--- BackEnd/src/main/java/com/nateroe/photo/dao/AbstractDao.java
+++ BackEnd/src/main/java/com/nateroe/photo/dao/AbstractDao.java
@@ -3,19 +3,14 @@ package com.nateroe.photo.dao;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.GenericTypeResolver;
 
 import com.nateroe.photo.model.CommonName;
 
-@Stateless
 public abstract class AbstractDao<T, ID extends Serializable> {
-	private static final Logger LOGGER = LoggerFactory.getLogger(AbstractDao.class);
 	protected Class<T> entityClass;
 
 	@PersistenceContext(unitName = "NatePhotoDB")
@@ -24,7 +19,6 @@ public abstract class AbstractDao<T, ID extends Serializable> {
 	@SuppressWarnings("unchecked")
 	public AbstractDao() {
 		// find the Class of T
-
 		this.entityClass = (Class<T>) GenericTypeResolver.resolveTypeArguments(getClass(),
 				AbstractDao.class)[0];
 	}
@@ -38,10 +32,6 @@ public abstract class AbstractDao<T, ID extends Serializable> {
 	}
 
 	public T findByPrimaryKey(ID id) {
-
-//		EntityManagerFactory factory = Persistence.createEntityManagerFactory("NatePhotoDB");
-//		em = factory.createEntityManager();
-
 		return em.find(entityClass, id);
 	}
 
