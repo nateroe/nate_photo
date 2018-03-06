111b0e3468e81b940caf99b2c9b1386004a5a7a1
nate <phlake@gmail.com>
2018-03-06 12:56:14 -0800

    entity id is not @XmlTransient

--- BackEnd/DataModel/src/main/java/com/nateroe/photo/model/AbstractEntity.java
+++ BackEnd/DataModel/src/main/java/com/nateroe/photo/model/AbstractEntity.java
@@ -5,11 +5,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.SequenceGenerator;
-import javax.xml.bind.annotation.XmlTransient;
 
 @MappedSuperclass
 public abstract class AbstractEntity {
-	@XmlTransient
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "increment")
 	@SequenceGenerator(name = "increment", sequenceName = "CommonName_pk_seq")
