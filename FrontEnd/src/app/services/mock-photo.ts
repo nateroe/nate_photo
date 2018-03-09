d5ee1786d18f224f8636ecdb8672819db942e7bd
nate <phlake@gmail.com>
2018-03-09 15:22:31 -0800

    gpl headers

--- FrontEnd/src/app/services/mock-photo.ts
+++ FrontEnd/src/app/services/mock-photo.ts
@@ -1,3 +1,22 @@
+/**
+ * NatePhoto - A photo catalog and presentation application.
+ * Copyright (C) 2018 Nathaniel Roe
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contact nate [at] nateroe [dot] com
+ */
 import { Photo } from "../model/photo";
 
 export const PHOTO: Photo = Object.assign( new Photo(), {
--- FrontEnd/src/app/services/photo.service.spec.ts
+++ FrontEnd/src/app/services/photo.service.spec.ts
@@ -1,15 +1,34 @@
+/**
+ * NatePhoto - A photo catalog and presentation application.
+ * Copyright (C) 2018 Nathaniel Roe
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Contact nate [at] nateroe [dot] com
+ */
 import { TestBed, inject } from '@angular/core/testing';
 
 import { PhotoService } from './photo.service';
 
-describe('PhotoService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [PhotoService]
-    });
-  });
+describe( 'PhotoService', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule( {
+            providers: [PhotoService]
+        } );
+    } );
 
-  it('should be created', inject([PhotoService], (service: PhotoService) => {
-    expect(service).toBeTruthy();
-  }));
-});
+    it( 'should be created', inject( [PhotoService], ( service: PhotoService ) => {
+        expect( service ).toBeTruthy();
+    } ) );
+} );
